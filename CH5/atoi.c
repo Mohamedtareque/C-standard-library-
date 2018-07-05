999adcaaf433b3a2221b317b02fe6533dd78874f
KoToZ <mohamedtareck95@gmail.com>
2018-07-05 07:12:59 +0200

    Create atoi.c

---
+++ CH5/atoi.c
@@ -0,0 +1,20 @@
+#define         MAXSIZE         30
+
+char buffer[MAXSIZE] = "123";
+
+int main(void)
+{
+    int num = atoii(buffer);
+    printf("%d",num+1);
+    return 0;
+}
+
+int atoii(char *s) // "123" => 100 + 20 + 3 --> n * 10 *s
+{
+    int n = 0 ;
+    while(*s >= '0' && *s <= '9' && *s!='\0' )
+    {
+        n = (*s++ - '0' ) + (10 * n)  ;
+    }
+    return n;
+}
