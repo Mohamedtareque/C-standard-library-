767ad516077bd72c73d93c053a9d176538d452dd
KoToZ <mohamedtareck95@gmail.com>
2018-07-11 09:14:39 +0200

    Update atoi.c

--- CH5/atoi.c
+++ CH5/atoi.c
@@ -14,7 +14,7 @@ int atoii(char *s) // "123" => 100 + 20 + 3 --> n * 10 *s
     int n = 0 ;
     while(*s >= '0' && *s <= '9' && *s!='\0' )
     {
-        n = (*s++ - '0' ) + (10 * n)  ;
+        n = (*s++ - '0' ) + (10 * n)  ;// covert string to number
     }
-    return n;
+    return n; 
 }
